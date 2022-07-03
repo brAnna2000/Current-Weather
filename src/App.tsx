a86c3013df9be3add52634832061e952d3b4840b
Anna <anya.baranovskaya@gmail.com>
2022-07-03 18:26:29 +0300

    Setting up redux store

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import './App.css';
-import FavoriteCitiesList from './Components/FavoriteCitiesList/index';
+import {useDispatch, useSelector} from 'react-redux';
+import FavoriteCitiesList from './Components/FavoriteCitiesList/index.tsx';
+
 interface AppProps {
     citiesList: string[],
 }
-function App({citiesList = ['Минск','Гродно','Витебск','Смолевичи','Молодечно','Лида','Барановичи','Борисов']}:AppProps) {
+function App({citiesList = []}:AppProps) {
+    const dispatch = useDispatch();
+    const cities: string[] = useSelector(state => state.cities);
+    citiesList = cities;
+    const addCash = (city: string | null) => {
+        dispatch({type: 'ADD_CITY', payload: city});
+    };
+    const getCash = (city: string | null) => {
+        dispatch({type: 'REMOVE_CITY', payload: city});
+    };
     return (
     <div className="App">
         <FavoriteCitiesList {...citiesList}/>
+        <button type='button' onClick={()=> addCash(prompt())}>Добавить город</button>
+        <button type='button' onClick={()=> getCash(prompt())}>Удалить город</button>
     </div>
     );
 }
-export default (App)
+export default (App);
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import App from './App.tsx';
 
+const defaultState={cities: []};
+const ADD_CITY = 'ADD_CITY';
+const REMOVE_CITY = 'REMOVE_CITY';
+
+const reducer = (state = defaultState, action: {type: string, payload?: any}) => {
+    switch(action.type){
+        case ADD_CITY:
+            return {...state}
+        case REMOVE_CITY:
+            return {...state}
+        default:
+            return state;
+    }
+}
+
+const store = createStore(reducer);
+
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+    <Provider store={store}>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>    
+    </Provider>,
     document.getElementById('root')
 );
